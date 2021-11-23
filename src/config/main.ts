145cfcbb098dae9c0f2c624c34abd316445e994d
Marek Mahut <marek.mahut@fivebinaries.com>
2021-11-23 15:57:20 +0100

    feat(main): adding NUTS testing

--- src/config/main.ts
+++ src/config/main.ts
@@ -12,6 +12,8 @@ export const POOLS = {
     {
       address:
         'addr_test1qpktdfrey07xa2shqe8vjn6rl4mh4xmspccjw3mcvgu67xdk0tx7w22wsvj28pjnx4gygulgex4um9ke3hckwvk8tm9s66tglc',
+      address:
+        'addr_test1qz2fxv2umyhttkxyxp8x0dlpdt3k6cwng5pxj3jhsydzer3jcu5d8ps7zex2k2xt3uqxgjqnnj83ws8lhrn648jjxtwq2ytjqp',
     },
   ],
 };
